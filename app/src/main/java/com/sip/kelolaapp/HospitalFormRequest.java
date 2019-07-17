9b9cb9d84c2ce7d6cdcaf5d857d325811369b43f
earth1991 <wahab.sukoraharjo@gmail.com>
2019-07-17 11:40:37 +0700

    pop up

--- app/src/main/java/com/sip/kelolaapp/HospitalFormRequest.java
+++ app/src/main/java/com/sip/kelolaapp/HospitalFormRequest.java
@@ -1,28 +1,37 @@
 package com.sip.kelolaapp;
 
 import android.app.Activity;
+import android.app.Dialog;
 import android.content.Intent;
 import android.drm.DrmStore;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class HospitalFormRequest extends Activity
 {
 
     private Button btn_order;
-
+    Dialog myDialog;
     protected void onCreate(Bundle savedInstanceState)
     {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.hospital_form_request);
-        action();
+        myDialog = new Dialog(this);
     }
 
-    private void action()
+
+
+    public void Order(View v)
     {
-        btn_order=(Button)findViewById(R.id.btn_order_form);
+        TextView txtclose;
+        myDialog.setContentView(R.layout.hospital_popup_order);
+        btn_order = (Button) myDialog.findViewById(R.id.btn_struck);
         btn_order.setOnClickListener(new View.OnClickListener()
         {
             @Override
@@ -34,7 +43,17 @@ public class HospitalFormRequest extends Activity
 
             }
         });
-
+        txtclose =(TextView) myDialog.findViewById(R.id.txtclose);
+        txtclose.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                myDialog.dismiss();
+            }
+        });
+        myDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        myDialog.show();
     }
 
 
