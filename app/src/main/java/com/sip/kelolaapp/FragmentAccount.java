06a8b98a8165f7503949ab9a157b7eaa75c99b5a
earth1991 <wahab.sukoraharjo@gmail.com>
2019-08-20 19:12:20 +0700

    update dashboard

--- app/src/main/java/com/sip/kelolaapp/FragmentAccount.java
+++ app/src/main/java/com/sip/kelolaapp/FragmentAccount.java
@@ -1,10 +1,12 @@
 package com.sip.kelolaapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.text.Editable;
@@ -15,15 +17,32 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
 public class FragmentAccount extends Fragment
 {
+    private CardView btn_logout;
+    private SessionManager session;
     public View onCreateView( LayoutInflater inflater,  ViewGroup container,  Bundle savedInstanceState)
     {
         View view = inflater.inflate(R.layout.fragment_account, container,false);
         TextView nama_dashboard =(TextView) view.findViewById(R.id.textAccount);
+        session = new SessionManager(getActivity().getApplicationContext());
+        String nama = session.isNama();
+
+        btn_logout = (CardView) view.findViewById(R.id.logout_admin);
+        btn_logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                session.setLogin(false);
+                Intent i = new Intent(getActivity(), MainActivity.class);
+                getActivity().startActivity(i);
+                getActivity().finish();
+            }
+        });
+
         return view;
     }
 
